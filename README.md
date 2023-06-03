c3af8826c4bd4f134a83291d984099e3a311d1f2
smsxgli <smsxgli@gmail.com>
2023-06-03 16:18:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HOW TO SETUP
+in the root of this project,
+```shell
+# setup Debug configure 
+cmake -S . -B build/Debug -DCMAKE_BUILD_TYPE=Debug -G Ninja
+# now build it!
+cmake --build build/Debug
+```
---
+++ cmake/common_opt.cmake
@@ -0,0 +1,50 @@
+include(CheckIPOSupported)
+
+add_library(
+    common_opt
+    INTERFACE
+)
+
+if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+# not consider clang, for now
+    target_compile_options(
+        common_opt
+        INTERFACE
+        # warning
+        -Wall
+        -Wextra
+        -Wconversion
+        -Wpedantic
+        -Wformat
+        -Werror=format-security
+        # message
+        -fmessage-length=0
+        -fdiagnostics-color=always
+        # safety
+        $<$<CONFIG:Release>:-Wp,-D_FORTIFY_SOURCE=2>
+        -fstack-protector-strong
+        -fstack-clash-protection
+        -fexceptions
+        -fasynchronous-unwind-tables
+        # output
+        -fno-common
+        -MMD
+        -MP
+        # misc
+        -pipe
+        -fno-plt
+        # sanitizer
+        $<$<CONFIG:Debug>:-fsanitize=address,undefined>
+        $<$<CONFIG:Debug>:-fno-omit-frame-pointer>
+        $<$<CONFIG:Debug>:-O1>
+    )
+    target_link_options(
+        common_opt
+        INTERFACE
+        $<$<CONFIG:Debug>:-fsanitize=address,undefined>
+        -Wl,-O1,--as-needed,-z,relro,-z,now,-z,noexecstack
+    )
+endif()
+
+# compiler must support LTO
+check_ipo_supported()
